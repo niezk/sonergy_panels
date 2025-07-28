2d6a16a1046a512e8da2051ed6fc1ccb13ca7f31
niezk <119299732+niezk@users.noreply.github.com>
2025-07-28 20:43:17 +0700

    fix api firebase

--- src/assets/firebase-config.ts
+++ src/assets/firebase-config.ts
@@ -2,8 +2,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Initialize Firebase
-const firebaseKey = import.meta.env.FIREBASE_APP
-initializeApp(firebaseKey);
+const firebase_key_local={
+  apiKey: "AIzaSyDw7RlRTfWhBgepVFsZdkOLBculX66S2q8",
+  authDomain: "dummy-electic-consume.firebaseapp.com",
+  projectId: "dummy-electic-consume",
+  storageBucket: "dummy-electic-consume.firebasestorage.app",
+  messagingSenderId: "140474622883",
+  appId: "1:140474622883:web:c9dbaca4dbcaf6e5d7f016",
+  measurementId: "G-KYL148P57Z"
+};
+
+
+initializeApp(firebase_key_local);
 const db = getFirestore()
 export default db
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || "/",
   plugins: [
     vue(),
     vueJsx(),
